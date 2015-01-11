7a0ea38565044d6c0b269ab6abb60a749ba7158a
Arata <tarata43@yahoo.co.jp>
2015-01-12 00:03:52 +0900

    clone失敗時

--- script/install/vagrant/install-application.sh
+++ script/install/vagrant/install-application.sh
@@ -1,18 +1,31 @@
 #! /bin/sh
 
 password=`cat /vagrant/script/keys/password_for_private_key`
-repo=git@github.com:tarata/dotfiles.git
+repo=git@github.com:tarata/expect-command-getting-started.git
+
+expect_message_ssh="Are you sure you want to continue connecting (yes/no)?"
+expect_message_key_password="Enter passphrase for key '/home/vagrant/.ssh/private_key_for_github': "
 
 expect -c "
 set timeout -1
 spawn git clone $repo
-expect \"Are you sure you want to continue connecting (yes/no)?\" {
+expect \"${expect_message_ssh}\" {
     send \"yes\n\"
-    expect \"Enter passphrase for key '/home/vagrant/.ssh/private_key_for_github':\"
+    expect \"${expect_message_key_password}\"
     send \"${password}\n\"
-} \"Enter passphrase for key '/home/vagrant/.ssh/private_key_for_github':\" {
+
+    expect \"${expect_message_key_password}\" {
+        set timeout 1
+    } \"*$\";
+} \"${expect_message_key_password}\" {
     send \"${password}\n\"
+    expect \"${expect_message_key_password}\" {
+        set timeout 1
+    } \"*$\";
 }
-interact
 "
 
+if [ ! -d $HOME/`basename ${repo%.git}` ]; then
+    echo "\n"
+    echo "failed to git clone"
+fi
